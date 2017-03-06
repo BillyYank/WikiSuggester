2e5fa0470a8fd14f52b59a253a6630593f10418e
Тарас Стасюк <taras-st@yandex-team.ru>
2017-03-07 01:05:21 +0300

    class Parser2.0

--- parser.py
+++ parser.py
@@ -5,19 +5,34 @@ import urllib2
 import networkx as nx
 from lxml import html
 import re
+import urllib
 
 
 class Parser:
-    def get_url_from_name(self, name):
-        return "https://en.wikipedia.org/wiki/" + name
+    def filter(self, url):
+        bad_words = [
+                     'Main_Page',
+                     ':',
+                     ".org",
+                     "International_Standard_Book_Number"
+                     ]
+                     for word in bad_words:
+                         if url.find(word) != -1:
+                             return False
+                         return True
+
+def get_url_from_name(self, name):
+    return "https://en.wikipedia.org/wiki/" + name
     
     def get(self, name):
         url = self.get_url_from_name(name)
         page = urllib2.urlopen(url).read()
         soup = bs(page, 'html.parser')
         refs = soup.findAll('a', href=re.compile('/wiki/*'))
-        for ref in refs:
-            yield str(ref).split('/')[2].split("\"")[0]
+        links = map(lambda ref: ref.get('href'), refs)
+        links = filter(self.filter, links)
+        for link in links:
+            print link.split('/')[2]
 
 
 parser = Parser()
